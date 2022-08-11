bc11f9e4d6a0278251e5d5773fba91c03999570d
Freddiiy <frederikgaller@live.dk>
2022-08-11 19:32:17 +0200

    made character work and added media endpoints and list of mounts endpoint.

--- routers/character.py
+++ routers/character.py
@@ -17,8 +17,32 @@ async def test():
 @router.get("/{region}/{slug}/{char_name}")
 async def get_character(region: str, slug: str, char_name: str):
     params = {
-        "namespace": "static-eu",
+        "namespace": f"profile-{region}",
         "locale": "en_US",
     }
 
     return utils.blizzard.get_data_from_api(region, f"/profile/wow/character/{slug}/{char_name}", params)
+
+
+@router.get("/mounts/{region}/{slug}/{char_name}")
+async def get_character_mounts(region: str, slug: str, char_name: str):
+    params = {
+        "namespace": f"profile-{region}",
+        "locale": "en_US",
+    }
+
+    return utils.blizzard.get_data_from_api(region,
+                                            f"/profile/wow/character/{slug}/{char_name}/collections/mounts",
+                                            params)
+
+
+@router.get("/media/{region}/{slug}/{char_name}")
+async def get_character_media(region: str, slug: str, char_name: str):
+    params = {
+        "namespace": f"profile-{region}",
+        "locale": "en_US",
+    }
+
+    return utils.blizzard.get_data_from_api(region,
+                                            f"/profile/wow/character/{slug}/{char_name}/character-media",
+                                            params)
